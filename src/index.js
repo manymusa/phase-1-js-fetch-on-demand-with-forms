c916159a44d5a73136d33613917ab8c204cfc5d1
manymusa <adanalin113@gmail.com>
2022-08-20 22:25:21 -0400

    commit tree 0f43a01aa7488a02d0ee94e01a1fc4c5b0d14e5f

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit',(e) => {
+        e.preventDefault();
+        const formTextInput = document.querySelector('input#searchByID');
+        console.log(formTextInput.value);
+        fetch(`http://localhost:3000/movies/${formTextInput.value}`)
+        .then(res=> res.json())
+        .then(json => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+            title.textContent = json.title;
+            summary.textContent = json.summary;
+        }) 
+    })
   
 }
 
